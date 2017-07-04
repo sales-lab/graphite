869a33a3f37725a768730c46135c5ec064e710fa
Gabriele Sales <gbrsales@gmail.com>
2017-07-04 10:20:15 +0200

    Banner for test_fetch.R

--- tests/testthat/test_fetch.R
+++ tests/testthat/test_fetch.R
@@ -1,4 +1,22 @@
-library(httr)
+# Copyright 2017 Gabriele Sales <gabriele.sales@unipd.it>
+#
+#
+# This file is part of graphite.
+#
+# graphite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License
+# version 3 as published by the Free Software Foundation.
+#
+# graphite is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with graphite. If not, see <http://www.gnu.org/licenses/>.
+
+suppressPackageStartupMessages(library(httr))
+
 context("Fetching of pathway data")
 
 test_that("Mus musculus pathways are available", {
