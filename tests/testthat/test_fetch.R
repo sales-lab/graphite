5980ca57fa34ce8e37b8855fba081adcd3720f53
Gabriele Sales <gbrsales@gmail.com>
2021-07-15 12:39:16 +0200

    Stronger test for pathway download

--- tests/testthat/test_fetch.R
+++ tests/testthat/test_fetch.R
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 Gabriele Sales <gabriele.sales@unipd.it>
+# Copyright 2017-2021 Gabriele Sales <gabriele.sales@unipd.it>
 #
 #
 # This file is part of graphite.
@@ -19,8 +19,8 @@ suppressPackageStartupMessages(library(httr))
 
 context("Fetching of pathway data")
 
-test_that("Mus musculus pathways are available", {
-  url <- remoteUrl("mmusculus-reactome")
-  r <- HEAD(url)
-  expect_equal(http_status(r)$category, "Success")
+test_that("Mus musculus pathways can be downloaded", {
+  purgeCache()
+  p <- pathways("mmusculus", "kegg")
+  expect_s4_class(p, "PathwayList")
 })
