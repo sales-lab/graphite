ba5be12cae0d773d23289689795d650506bd2f60
Gabriele Sales <gbrsales@gmail.com>
2023-04-19 11:14:01 +0200

    Updated all pathway data

--- R/fetch.R
+++ R/fetch.R
@@ -55,7 +55,7 @@ pathwayDatabases <-function() {
   sscrofa=c("kegg", "reactome", "wikipathways"),
   xlaevis=c("kegg"))
 
-.version <- 19
+.version <- 20
 
 
 loadData <- function(name, retry = TRUE) {
